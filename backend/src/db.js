3664280824fcc2654986a2eac5b67102dc9e0f98
Margarita Yaganova <m.yaganova@bi.zone>
2026-03-14 21:16:21 +0300

    Fix db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const Database = require('better-sqlite3');
 
-const path = require('path');
-const Database = require('better-sqlite3');
-
 const dbPath = process.env.DB_PATH || path.join(__dirname, '..', 'data', 'app.db');
 const db = new Database(dbPath);
 
